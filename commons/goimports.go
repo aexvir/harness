92452c129325272a8e1e5b7172d01d8bfd1f6949
HandsomeJack <dusek.honza@gmail.com>
2025-02-02 15:26:04 +0100

    chore(commons): extend GoImports with GoImportsOpts
    Similar to golangci-lint implementation, we should support changing the
    default behavior of goimports by providing a GoImportsOpts struct.
    
    Start with default (latest) version override.

--- commons/goimports.go
+++ commons/goimports.go
@@ -10,11 +10,19 @@ import (
 
 // GoImports formats code sorting imports taking in account the
 // local package supplied as argument.
-func GoImports(localpkg string) harness.Task {
+func GoImports(localpkg string, opts ...GoImportsOpt) harness.Task {
+	conf := goimportsconf{
+		version: "latest",
+	}
+
+	for _, opt := range opts {
+		opt(&conf)
+	}
+
 	return func(ctx context.Context) error {
 		imp, _ := bintool.NewGo(
 			"golang.org/x/tools/cmd/goimports",
-			"latest",
+			conf.version,
 		)
 
 		if err := imp.Ensure(); err != nil {
@@ -22,5 +30,19 @@ func GoImports(localpkg string) harness.Task {
 		}
 
 		return harness.Run(ctx, imp.BinPath(), harness.WithArgs("-w", "-local", localpkg, "."))
+	}
+}
+
+type goimportsconf struct {
+	version string
+}
+
+type GoImportsOpt func(c *goimportsconf)
+
+// WithGoImportsVersion allows specifying the goimports version
+// that should be used when running this task.
+func WithGoImportsVersion(version string) GoImportsOpt {
+	return func(c *goimportsconf) {
+		c.version = version
 	}
 }
