6a55f4b4a9839e0018dfe89502f7379956559d6d
HandsomeJack <dusek.honza@gmail.com>
2025-08-24 00:44:03 +0200

    feat(commons): add OnlyOnWindows task filter

--- commons/envdetect.go
+++ commons/envdetect.go
@@ -3,6 +3,7 @@ package commons
 import (
 	"context"
 	"os"
+	"runtime"
 
 	"github.com/aexvir/harness"
 )
@@ -29,9 +30,25 @@ func OnlyLocally(task harness.Task) harness.Task {
 	return task
 }
 
+// OnlyOnWindows returns the task specified as argument only in the case
+// the current OS Windows.
+// Otherwise it returns a noop task.
+func OnlyOnWindows(task harness.Task) harness.Task {
+	if !IsWindows() {
+		return noop
+	}
+
+	return task
+}
+
 // IsCIEnv returns true if the current environment is a known ci system.
 func IsCIEnv() bool {
 	return os.Getenv("CI") != ""
+}
+
+// IsWindows returns true if the current environment is Windows.
+func IsWindows() bool {
+	return runtime.GOOS == "windows"
 }
 
 func noop(ctx context.Context) error { return nil }
